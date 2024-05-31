a7d62f08ef557fbc73d8f35c3e07f020183f5aa0
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-05-31 12:36:41 -0700

    changed the title

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ import { footer } from '../constant'
 const Footer = () => {
   return (
     <section className='pt-16'>
-      <ul className='list-none flex flex-wrap items-center justify-center'>
+      <ul className='list-none sm:flex flex-1 text-center  items-center justify-center'>
         {footer.map((item)=>(
-            <li key={item.id} className='px-3 text-grey'>
+            <li key={item.id} className='px-3 py-3 text-grey'>
                 <a href="">{item.title}</a>
             </li>
         ))}
