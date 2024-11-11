fcf592fd4de1897bed104909437585141bdaefed
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-11-11 09:17:04 +0100

    removed react slide-down dep

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import { add, close } from '../assets'
-import {SlideDown} from 'react-slidedown';
-import 'react-slidedown/lib/slidedown.css';
+
 
 const FaqItem = ({ item, index }) => {
     const [isOpen, setIsopen] = useState(false);
@@ -13,9 +12,9 @@ const FaqItem = ({ item, index }) => {
             <img src={active ? close : add} alt="add" className='size-5 object-contain transition-colors duration-500' />
             <div className='h-6 text-white font-poppins font-normal text-xl max-lg:text-[18px] transition-colors duration-500 max-md:flex max-md:min-h-20 max-md:items-center'>{item.question}</div>
         </div>
-        <SlideDown>
-            {isOpen === item.id && <div className='px-7 py-3.5 text-[18px]'>{item.answer}</div>}
-        </SlideDown>
+          <div className='transition-all duration-500'>
+          {isOpen === item.id && <div className='font-poppins font-normal px-7 py-3.5 text-[18px]'>{item.answer}</div>}
+          </div>
         {index !== 5 && <div className='w-full h-[1px] mt-4 g4' />}
     </div>
   )
